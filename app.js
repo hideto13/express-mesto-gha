0ed6b7abe4b82d53dfa9dc7a78bf8bd794bbd566
Tanya <pinki_00@mail.ru>
2022-03-07 12:35:33 +0300

    Add routes and controllers

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const { PORT = 3000, BASE_PATH } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
   console.log('Ссылка на сервер:');
-  console.log(BASE_PATH);
 });
