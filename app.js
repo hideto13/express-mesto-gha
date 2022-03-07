124d70c2b2f6a0958087db4d0b5a700fafb712fd
Tanya <pinki_00@mail.ru>
2022-03-07 10:34:20 +0300

    Basic settings

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const { PORT = 3000, BASE_PATH } = process.env;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log('Ссылка на сервер:');
+  console.log(BASE_PATH);
+});
