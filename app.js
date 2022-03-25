b93ffa68bc0d6e0978ec8d403b1baa4a230106b6
hideto13 <pinki_00@mail.ru>
2022-03-25 17:02:26 +0300

    add celebrate

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, errors, Joi } = require('celebrate');
 const auth = require('./middlewares/auth');
 const {
   createUser,
@@ -16,9 +17,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.post('/signup', createUser);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+    name: Joi.string().min(2).max(30),
+    avatar: Joi.string().min(10),
+    about: Joi.string().min(2).max(60),
+  }),
+}), createUser);
 
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), login);
 
 app.use('/users', auth, require('./routes/users'));
 
@@ -27,6 +41,8 @@ app.use('/cards', auth, require('./routes/cards'));
 app.use(auth, (req, res, next) => {
   next(new NotFoundError('Некорректный запрос'));
 });
+
+app.use(errors());
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
